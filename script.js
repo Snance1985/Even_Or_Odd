3eb662a6726e96b3dca5db0a19267693c7be0c3e
Sean Nance <sean_nance@hotmail.com>
2023-11-28 19:33:38 -0800

    created script.js file for even_or_odd

---
+++ script.js
@@ -0,0 +1,14 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+// Example usage:
+var result1 = evenOrOdd(7);
+console.log(result1); // Output: Odd
+
+var result2 = evenOrOdd(12);
+console.log(result2); // Output: Even
